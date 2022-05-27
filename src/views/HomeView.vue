76b60755749619c88684b935535878d4c9cfcda8
Aria Nikzad <a.c.nikzad@gmail.com>
2022-05-26 19:09:28 -0700

    got the API to work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios';
+
+// var limit = 1;
+
+// axios.get('https://api.api-ninjas.com/v1/facts?limit=' + limit,).then(response => {console.log(response.data);
+// })
+// .catch(error => console.error(error));
+
+// mounted(); {
+//   this.getFacts();
+// };
+
+// methods:{
+//   getFacts() {
+//    axios.get(`https://api.api-ninjas.com/v1/facts?limit=2`,
+//     {headers: key: "Mmp58EM5mhRBF+5Bh75fcA==VIfI25pl51Ng9oYh"
+//     }).then(response => {
+//       console.log (response);
+//     };
+//   }
+
+
+
+
+
+
+
+  //  ({
+  //   method: "GET",
+  //   url: "https://api.api-ninjas.com/v1/facts?limit=2",
+  //   headers:{
+  //     key: "Mmp58EM5mhRBF+5Bh75fcA==VIfI25pl51Ng9oYh"}
+  //     ).then (response => {
+  //     console.log (response);
+  //   };
+  //   });
+  // }
+
+
+// Vue.ajax({
+//     method: 'GET',
+    // url: 'https://api.api-ninjas.com/v1/facts?limit=' + limit,
+//     headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+
+  mounted() {
+    axios.get('https://api.api-ninjas.com/v1/facts?limit=2', {
+      headers: {'X-Api-Key': 'Mmp58EM5mhRBF+5Bh75fcA==VIfI25pl51Ng9oYh'}
+    })
+    .then((response) => {
+      console.log(response)
+    })
+  },
+
+  methods:{
+
+    // var config = {
+    //   headers: {
+    //     'key': 'https://api.api-ninjas.com/v1/facts?limit=2'
+    //   }
+    // }
+    // getFacts() {
+    //  axios.get(`https://api.api-ninjas.com/v1/facts?limit=2`, config).then(response => {
+    //     console.log (response);
+    //   };
+    }
+
+
   }
-}
 </script>
