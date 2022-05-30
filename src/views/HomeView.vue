9525bee9a313590fbb14085dde08f474c6fd111e
Aria Nikzad <a.c.nikzad@gmail.com>
2022-05-30 13:31:44 -0700

    added the lie

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <template>
   <body>
     <div class="home">
-     <h1><span id="factOne"></span></h1>
-     <h1><span id="factTwo"></span></h1>
-      <h1 id="lie"></h1>
-      <!-- <button v-on:click="getFacts()">Click Me</button> -->
+      <ul>
+      <li><button><span id="factOne"></span></button></li>
+      <li><button><span id="factTwo"></span></button></li>
+      <li><button id="lie"><span id="lie"></span></button></li>
+      </ul>
+      <button v-on:click="getFacts()">Click Me</button>
     </div>
   </body>
 </template>
@@ -12,15 +14,13 @@
 <script>
 import axios from 'axios';
 
-  // document.getElementById('factOne').innerHTML = factOne;
-  // document.getElementById('factTwo').innerHTML = factTwo;
-
 export default {
 
   data() {
     return {
       factOne: "",
       factTwo: "",
+
     }
   },
 
@@ -36,15 +36,31 @@ export default {
       headers: {'X-Api-Key': 'Mmp58EM5mhRBF+5Bh75fcA==VIfI25pl51Ng9oYh'}
     })
     .then((response) => {
+      let x = 0;
       let result = response.data;
       let factOne = JSON.stringify(result[0])
       let factTwo = JSON.stringify(result[1])
+      let fakeFacts = [
+        "Thomas Edison, the inventor of the refrigerator was afraid of the rats",
+        "Mercury has 16 moons",
+        "Reptiles perspire, and have 4 sweat glands",
+        "The planet Neptune spins opposite to the other planets in the solar system",
+        "Thomas Edison was offered the presidency of Palestine in 1937, but he declined",
+        "The New York Public Library, located in New York City, is the largest library in the world",
+        "Men smile more than women do",
+       ]
+
+       let lie = fakeFacts[x]
+       x = Math.floor(Math.random()*fakeFacts.length)
+       console.log(x)
 
       factOne = factOne.substring(9, factOne.length-2);
       factTwo = factTwo.substring(9, factTwo.length-2);
+
       
       document.getElementById('factOne').innerHTML = factOne;
       document.getElementById('factTwo').innerHTML = factTwo;
+      document.getElementById('lie').innerHTML = lie;
 
     })
 
